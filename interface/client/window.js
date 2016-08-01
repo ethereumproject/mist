92560272b4eb6151e0aabd6ba0b292ee7c2da929
gravity <your_email@example.com>
2016-08-02 00:08:41 +0200

    Moved packages from atmosphere to submodules within the project so it is easier to manage

--- interface/client/window.js
+++ interface/client/window.js
@@ -1,22 +1,16 @@
-
 // add the platform to the HTML tag
 setTimeout(function(){
     document.getElementsByTagName('html')[0].className =  window.mist.platform;
-
     if (window.basePathHref) {
       var base = document.createElement('base');
-
       base.href = window.basePathHref;
-      
-      document.getElementsByTagName('head')[0].appendChild(base);      
+      document.getElementsByTagName('head')[0].appendChild(base);
     }
-
 }, 100);
 
-
-$(window).on('blur', function(e){ 
+$(window).on('blur', function(e){
     $('body').addClass('app-blur');
 });
-$(window).on('focus', function(e){ 
+$(window).on('focus', function(e){
     $('body').removeClass('app-blur');
 });

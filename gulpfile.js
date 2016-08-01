b863e45d27fd40439aa11bcd56ccc12c4818c792
gravity <your_email@example.com>
2016-08-01 05:57:11 +0200

    gulpfile.js - Direct the auxillary libs to the ethereumproject repositories.

--- gulpfile.js
+++ gulpfile.js
@@ -273,9 +273,9 @@ gulp.task('bundling-interface', ['clean:dist', 'copy-files'], function(cb) {
                 cb(err);
             });
         } else {
-            console.log('Pulling https://github.com/ethereum/meteor-dapp-wallet/tree/'+ options.walletSource +' "'+ options.walletSource +'" branch...');
+            console.log('Pulling https://github.com/ethereumproject/meteor-dapp-wallet/tree/'+ options.walletSource +' "'+ options.walletSource +'" branch...');
             exec('cd interface/ && meteor-build-client ../dist_'+ type +'/app/interface/ -p "" &&'+
-                 'cd ../dist_'+ type +'/ && git clone --depth 1 https://github.com/ethereum/meteor-dapp-wallet.git && cd meteor-dapp-wallet/app && meteor-build-client ../../app/interface/wallet -p "" && cd ../../ && rm -rf meteor-dapp-wallet', function (err, stdout, stderr) {
+                 'cd ../dist_'+ type +'/ && git clone --depth 1 https://github.com/ethereumproject/meteor-dapp-wallet.git && cd meteor-dapp-wallet/app && meteor-build-client ../../app/interface/wallet -p "" && cd ../../ && rm -rf meteor-dapp-wallet', function (err, stdout, stderr) {
                 console.log(stdout);
                 console.log(stderr);
                 cb(err);
